2ea6faefd1b8c3cffd4d08631e55ce33d6d33cb0
rajabca04 <rajabca201804@gmail.com>
2023-06-16 20:39:50 +0530

    Gate ready to code

---
+++ javascript/Question_13.06.023.js
@@ -0,0 +1,19 @@
+// 1) WAP to print area of circle
+
+/*
+var radius = 5
+var PI = 3.14
+var Area = 0
+
+Area = PI * radius *radius
+console.log("Area = " + Area)
+------------------------------
+Output is. Area = 78.5
+*/
+
+// 2) WAP to 
+
+
+// Math
+
+console.log(Math.floor(1.2))
---
+++ javascript/basic.js
@@ -0,0 +1,110 @@
+// Constant\Literals:
+//     used when data is fixed
+//     boolean: ture false
+//     number: 10 , +10, -30, 34.54, +45.42, -45.53
+//     string: 'sample' , "sample", `raja
+//                                      kumar`
+                                    
+// Output Instruction: 
+//     console.log
+
+
+// var a = 10;
+// var b = 10.45
+// var c = 'Hello world'
+// var d = "Ducat Education Noida"
+// var e = `
+//         Hi i am
+//         raja kumar
+//         `
+// var f = false
+// var t = true
+
+// console.log(a,typeof(a));
+// console.log(b,typeof(b));
+// console.log(c,typeof(c));
+// console.log(d,typeof(d));
+// console.log(e,typeof(e));
+// console.log(f,typeof(f));
+// console.log(t,typeof(t));
+
+// var a = 23
+// var b = 48
+
+// var sum = a + b
+
+// console.log(a,"+",b,"=",sum)
+
+// console.log(a+"+"+b+"="+sum)
+// // Sting template litrals
+// console.log(`${a} + ${b} = ${sum}`) // backtiks(``)
+
+// WAP to print
+
+// var p = 535555
+// var r = 12.36
+// var t = 5 
+// var si = p*r*t/100
+// var total = p +si
+// var emi = total/(t*12)
+// console.log("Principal Amount "+p)
+// console.log("Rate             "+r)
+// console.log("Time             "+t)
+// console.log("Simple Interest  "+si.toFixed(2))
+// console.log("Total Amount     "+total.toFixed(2))
+// console.log("EMI              "+emi.toFixed(2))
+
+
+
+// var l = 25
+// var b = 12
+// var h = 10
+// var area = 2*(l*b + b*h + h*l)
+
+// console.log("Area of Cuboid = " + area)
+
+// var a = 25 
+// var b = 75
+// console.log(`Before swapping a = ${a} and b = ${b}`)
+// var temp = a
+// a = b
+// b = temp
+// console.log(`After swapping a = ${a} and b = ${b}`)
+
+// a = a+b
+// b = a-b
+// a = a-b
+// console.log(`Swapping without 3rd variable a = ${a} and b = ${b}`)
+// console.log("Success!!!")
+// console.warn("warnning !!!")
+// console.error("Error here !!!")
+
+// var a = prompt("Enter the first no.")
+// document.write(`<h1>a = ${a} and typeof = ${typeof(a)}</h1>`)
+
+
+// var p = Number(prompt("Enter the Priciple amount : "))
+// var r = Number(prompt("Enter the Rate :"))
+// var t = Number(prompt("Enter the Total time in years :"))
+// var si = p*r*t/100
+// var total = p+si
+// var emi = total/(t*12)
+
+// document.write(`
+// <h1>Principle Amount = ${p} <br>
+//     Rate = ${r} <br>
+//     Time = ${t} <br>
+//     si = ${si}  <br>
+//     total = ${total} <br>
+//     EMI = ${emi}</h1>`) 
+
+// var a = 15*4/20*15%25/5%7*10%15*3-55%75/3*10*(6*2**6)
+// console.log(a)
+console.log(15+5)
+console.log(15+"5")
+console.log(15+"Hello world")
+console.log("5"+15)
+console.log("5"-15)
+console.log(5-"15")
+console.log("5"-"15")
+console.log(5-"Hello world") // NaN
