9dd8d4f2565cf5fc82e28f75c4865ace4f64e675
rajabca04 <rajabca201804@gmail.com>
2023-06-23 20:24:29 +0530

    change

--- javascript/Question_13.06.023.js
+++ javascript/Question_13.06.023.js
@@ -16,4 +16,118 @@ Output is. Area = 78.5
 
 // Math
 
-console.log(Math.floor(1.2))
+// console.log(Math.floor(1.2))
+
+//WAP to print area cuboid 2*(l*b +b*h + h*l)
+
+// var ch = prompt("Enter charater: ")
+// if(ch.length!=1)
+//     document.write(`'${ch}' is not Valid character`)
+// else{
+//     if(ch>='a' && ch<='z'){
+//         if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+//         document.write(`'${ch}' is lowercase vovel`)
+//         else
+//         document.write(`'${ch}' is lowercase consonent`)
+//     }
+// }
+// else if(ch>='A' && ch<='Z'){
+//     if(ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U')
+//     document.write(`'${ch}' is Upercase vovel`)
+//     else
+//     document.write(`'${ch}' is Upercase consonent`)
+
+// }
+// else if(ch>=0 && ch<=9){
+//     document.write(`'${ch}' is Digits`)
+
+// }
+// else if(ch ==' '){
+//     document.write(`SPace`)
+
+// }
+// else{
+//     document.write(`'${ch}' Spacial charecters`)
+
+// // }
+
+// var a = Numbar(prompt("ENter the 1st Number"))
+// var b = Numbar(prompt("ENter the 2nd Number"))
+// var ch = prompt("Press + for Addition \n Press - for Subtraction \n press * for multiplication \n press / for Division \n press ** for squre ")
+
+// switch(ch){
+//     case "+":
+//         document.write(`${a} + ${b} = ${a+b}`)
+//         break
+//     case "-":
+//         document.write(`${a} - ${b} = ${a-b}`)
+//     case "*":
+//         document.write(`${a} * ${b} = ${a*b}`)
+//     case "/":
+//         document.write(`${a} / ${b} = ${a/b}`)
+//     case "**":
+//         document.write(`${a} ** ${b} = ${a**b}`)
+//     default:
+//         document.write("Invalid choice..")
+//         ;
+// }
+
+// loop:-
+
+// for(var i=1;i<=10;i++){
+//     console.log(`Hello world ${i}`)
+// }
+
+//  for(var i=1;i<=10;++i){
+    //     console.log(`Hello world ${i}`)
+    // }
+    // for(var i=1;i<=10;i=i++){
+    //         console.log(`Hello world ${i}`)
+    //     }
+
+    // for(var i=1;i<=10;i++);
+    // {
+    //          console.log(`Hello world ${i}`)
+    //      }
+
+    // for(let i=1;i<=10;i++){
+    //     console.log(`Hello world ${i}`)
+    // }
+
+
+    // var i =1
+    // for(;i<=10;)
+    // {
+    //     console.log(i);
+    //     i++
+    // }
+
+    // var i = 1
+    // for(;;)
+    // {
+    //     console.log(i);
+    //     i++
+    // }
+
+    // var i = 1
+    // for(;;)
+    // {
+    //     if(i>10)
+    //     console.log(i)
+    //     break;
+    //     i++ 
+    // }
+
+    var num = Number(prompt("enter no."))
+    for(let i = 1;i<=10;i++){
+        document.write(`<p>${num} * ${num} = ${num*i} <br></p>`)
+    }
+
+    var start = Number(prompt("Enter the range start from"))
+    var end = Number(prompt("Enter End :"))
+
+    var sum = 0
+    for(let i = start; i<=end;i++){
+        sum = sum+i
+    }
+    document.write(`sum = ${sum}`)
--- javascript/basic.js
+++ javascript/basic.js
@@ -80,7 +80,7 @@
 // console.error("Error here !!!")
 
 // var a = prompt("Enter the first no.")
-// document.write(`<h1>a = ${a} and typeof = ${typeof(a)}</h1>`)
+// document.write(`<h2>a = ${a} and typeof = ${typeof(a)}</h2>`)
 
 
 // var p = Number(prompt("Enter the Priciple amount : "))
@@ -91,20 +91,110 @@
 // var emi = total/(t*12)
 
 // document.write(`
-// <h1>Principle Amount = ${p} <br>
+// <h2>Principle Amount = ${p} <br>
 //     Rate = ${r} <br>
 //     Time = ${t} <br>
 //     si = ${si}  <br>
 //     total = ${total} <br>
-//     EMI = ${emi}</h1>`) 
+//     EMI = ${emi}</h2>`) 
 
 // var a = 15*4/20*15%25/5%7*10%15*3-55%75/3*10*(6*2**6)
 // console.log(a)
-console.log(15+5)
-console.log(15+"5")
-console.log(15+"Hello world")
-console.log("5"+15)
-console.log("5"-15)
-console.log(5-"15")
-console.log("5"-"15")
-console.log(5-"Hello world") // NaN
+// console.log(15+5)
+// console.log(15+"5")
+// console.log(15+"Hello world")
+// console.log("5"+15)
+// console.log("5"-15)
+// console.log(5-"15")
+// console.log("5"-"15")
+// console.log(5-"Hello world") // NaN
+// console.log("Hello I am raja kumar sharma form Noida...");
+
+
+
+// Logical operators:
+
+// 0 , null , undefined , " "  these all are false and remaining is true..
+
+// console.log(10 && 20)  // true i.e 20
+// console.log(10 && 0)    // false i.e 0
+// console.log(0 && 20)    // false i.e 20
+// console.log(0 && 0)    // false i.e 0
+// console.log(" " && " ")    // false i.e empty string
+
+// console.log(10 || 20); // true i.e 20
+// console.log(10 || 0);  // true i.e 10
+// console.log(0 || 20 ); // true i.e 20
+// console.log(0 || 0);  // false  i.e 0
+// console.log(10 || " "); // false i.e empty
+// console.log(0 || " "); // false i.e 0
+
+// console.log(!10);  // false -> oposite
+// console.log(!0);    // true
+
+// Bitewise Operator 
+
+// let no = 10
+// if(no%2==0){
+//     console.log("even");
+// }else{
+//     console.log("odd");
+// }
+
+
+// Q1. Wap to check whether no. equal or not.
+
+// var a = Number(prompt("Enter your 1st no."))
+// var b = Number(prompt("Enter 2nd no."))
+
+// if(a==b)
+//     document.write(`${a} And ${b} is equal.`)
+// else
+//     document.write("No. is not equal")
+
+// Q2. Wap to check no. is parfact squre or not.
+
+// var a = 2.55
+
+// // var psqr = Math.sqrt(a)
+
+// console.log(a,typeof(a));
+
+// // console.log(psqr);
+
+// Q3. WAP to check whether a parson can be vote or not
+
+        // var parsonAge = Number(prompt("Enter your age.."))
+
+        // if(parsonAge >= 18){
+        //         document.write(`Congretulation..! 
+        //         You can vote`)
+        // }
+        // else{
+        //         document.write("Sorry you can't vote...!")
+        // }
+
+// Q4. WAP to check whether a Entered charactor is LowerCase vovel, UperCase vovel, LowerCase consonent, UperCase consonent , digits , space and spacial chractor.
+
+
+
+var a = Numbar(prompt("ENter the 1st Number"))
+var b = Numbar(prompt("ENter the 2nd Number"))
+var ch = prompt("Press + for Addition \n Press - for Subtraction \n press * for multiplication \n press / for Division \n press ** for squre ")
+
+switch(ch){
+    case "+":
+        document.write(`${a} + ${b} = ${a+b}`)
+        break
+    case "-":
+        document.write(`${a} - ${b} = ${a-b}`)
+    case "*":
+        document.write(`${a} * ${b} = ${a*b}`)
+    case "/":
+        document.write(`${a} / ${b} = ${a/b}`)
+    case "**":
+        document.write(`${a} ** ${b} = ${a**b}`)
+    default:
+        document.write("Invalid choice..")
+}
+
